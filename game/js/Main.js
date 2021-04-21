366bc2a4a88ef967548d9910a28854cb8b808a1d
tigerza117 <noppawanza1171@gmail.com>
2021-04-21 21:53:25 +0700

    refactor: 💡 move loader to private function

--- game/js/Main.js
+++ game/js/Main.js
@@ -1,33 +1,62 @@
-function Main() {
-    this.app = new PIXI.Application({
-        width: 1920,
-        height: 1080,
-        antialias: true,
-        transparent: false,
-        backgroundColor: 0xFF00FF,
-        resolution: 1,
-        forceCanvas: true,
-        autoResize: true,
-        view: document.getElementById("app"),
-    })
+class Main {
+    constructor() {
+        this.app = new PIXI.Application({
+            width: 1920,
+            height: 1080,
+            antialias: true,
+            transparent: false,
+            backgroundColor: 0xFF00FF,
+            resolution: 1,
+            forceCanvas: true,
+            autoResize: true,
+            view: document.getElementById("app"),
+        })
 
-    this.stage = this.app.stage
+        this.stage = this.app.stage
 
-    this.loader = Loader()
+        this.loader = PIXI.Loader.shared
 
-    this.SCROLL_SPEED = 10
+        this.SCROLL_SPEED = 10
 
-    this.loader.onComplete.add(() => {
-        this.spriteSheetLoaded()
-    })
+        this.loader.onComplete.add(() => {
+            this.spriteSheetLoaded()
+        })
 
-    this.spriteSheetLoaded = () => {
-        this.scroller = new Scroller(this.stage);
-        requestAnimationFrame(this.update);
+        this.spriteSheetLoaded = () => {
+            this.scroller = new Scroller(this.stage);
+            requestAnimationFrame(this.update);
+        }
+
+        this.update = () => {
+            this.scroller.moveViewportXBy(this.SCROLL_SPEED);
+            requestAnimationFrame(this.update);
+        };
+
+        this.#Load()
     }
 
-    this.update = () => {
-        this.scroller.moveViewportXBy(this.SCROLL_SPEED);
-        requestAnimationFrame(this.update);
-    };
+    #Load() {
+        this.loader.onLoad.add(() => {
+            console.log(`Loading...`)
+        })
+        this.loader.onProgress.add((data) => {
+            console.log(`Progress load ${data.progress}%`)
+        })
+        this.loader.onError.add((err) => {
+            console.log("ERROR LOAD", err)
+        })
+
+        const resources = [
+            {
+                name: "bg_water",
+                path: "bg_water.png"
+            },
+        ]
+
+        resources.forEach((resource) => {
+            this.loader.add(resource.name, `./resources/${resource.path}`)
+        })
+
+        this.loader.load();
+    }
 }
