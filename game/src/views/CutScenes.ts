a8c134006e90f71b3e040dbcfd124601db2676d9
tigerza117 <noppawanza1171@gmail.com>
2021-05-21 00:35:37 +0700

    feat: add promise to control cut scenes run

--- game/src/views/CutScenes.ts
+++ game/src/views/CutScenes.ts
@@ -21,36 +21,42 @@ export default class CutScenes extends IView {
 
     initial() {
         super.initial();
+    }
 
-        this.scenes.forEach(async (scene, index) => {
-            let sprite: PIXI.AnimatedSprite;
-            await setTimeout(() => {
-                let textureArray: PIXI.Texture[] = [];
+    start(): Promise<null> {
+        return new Promise(async (resolve) => {
+            for (let scene of this.scenes) {
+                let sprite: PIXI.AnimatedSprite;
+                await new Promise((resolve1) => {
+                    let textureArray: PIXI.Texture[] = [];
 
-                scene.frames.forEach((frame) => {
-                    let texture = this.app.loader.resources[frame].texture;
-                    textureArray.push(texture);
-                });
-                sprite = new PIXI.AnimatedSprite(textureArray);
-                sprite.alpha = 0;
-                let { w, h } = GAME_SIZE();
-                sprite.width = w;
-                sprite.height = h;
-                sprite.animationSpeed = 20 / scene.dpf;
-                this.addChild(sprite);
-                TweenMax.to(sprite, 0.2, {
-                    pixi: { alpha: 1 },
-                    ease: 'easeIn',
-                }).then(() => sprite.play());
-                setTimeout(() => {
-                    TweenMax.to(sprite, 0.2, {
-                        pixi: { alpha: 0 },
-                        ease: 'easeIn',
-                    }).then(() => {
-                        this.removeChild(sprite);
+                    scene.frames.forEach((frame) => {
+                        let texture = this.app.loader.resources[frame].texture;
+                        textureArray.push(texture);
                     });
-                }, scene.duration);
-            }, scene.duration * index);
+                    sprite = new PIXI.AnimatedSprite(textureArray);
+                    sprite.alpha = 0;
+                    let { w, h } = GAME_SIZE();
+                    sprite.width = w;
+                    sprite.height = h;
+                    sprite.animationSpeed = 20 / scene.dpf;
+                    this.addChild(sprite);
+                    TweenMax.to(sprite, 0.3, {
+                        pixi: { alpha: 1 },
+                        ease: 'easeIn',
+                    }).then(() => sprite.play());
+                    setTimeout(() => {
+                        TweenMax.to(sprite, 0.3, {
+                            pixi: { alpha: 0 },
+                            ease: 'easeOut',
+                        }).then(() => {
+                            this.removeChild(sprite);
+                            resolve1(0);
+                        });
+                    }, scene.duration);
+                });
+            }
+            resolve(null);
         });
     }
 }
